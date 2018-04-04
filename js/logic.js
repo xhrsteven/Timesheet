22e47f743695ef2a077504a85eb9f0950250efff
Steven <xhrsteven@gmail.com>
2018-04-03 20:08:35 -0500

    update

--- js/logic.js
+++ js/logic.js
@@ -10,39 +10,42 @@
         var database = firebase.database();
         // Initial Values
         var name = "";
-        var email = "";
-        var age = 0;
-        var comment = "";
+        var role = "";
+        var date = '';
+        var rate = "";
         // Capture Button Click
-        $("#add-user").on("click", function (event) {
+        $("#submit-bid").on("click", function (event) {
                 event.preventDefault();
 
             // Grabbed values from text-boxes
-            name = $("#name-input").val().trim();
-            email = $("#email-input").val().trim();
-            age = $("#age-input").val().trim();
-            comment = $("#comment-input").val().trim();
+            name = $("#employeeName-input").val().trim();
+            role = $("#role-input").val().trim();
+            date = $("#date-input").val().trim();
+            rate = $("#rate-input").val().trim();
             // Code for "Setting values in the database"
-            database.ref().set({
+            database.ref().push({
                 name: name,
-            email: email,
-            age: age,
-            comment: comment
+                role: role,
+                date: date,
+                rate: rate,
+                dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
     });
     // Firebase watcher + initial loader HINT: .on("value")
         database.ref().on("value", function (snapshot) {
                 // Log everything that's coming out of snapshot
-                console.log(snapshot.val());
-            console.log(snapshot.val().name);
-            console.log(snapshot.val().email);
-            console.log(snapshot.val().age);
-            console.log(snapshot.val().comment);
+            //     console.log(snapshot.val());
+            // console.log(snapshot.val().name);
+            // console.log(snapshot.val().email);
+            // console.log(snapshot.val().age);
+            // console.log(snapshot.val().comment);
             // Change the HTML to reflect
             $("#name-display").text(snapshot.val().name);
-            $("#email-display").text(snapshot.val().email);
-            $("#age-display").text(snapshot.val().age);
-            $("#comment-display").text(snapshot.val().comment);
+            $("#email-display").text(snapshot.val().role);
+            $("#age-display").text(snapshot.val().date);
+            $("#comment-display").text(snapshot.val().rate);
+            $('#totaltime-display').text()
+
             // Handle the errors
         }, function (errorObject) {
                 console.log("Errors handled: " + errorObject.code);
